37169b4bcc16883399814dd80848371c20e42b69
Lim, Chee Kin <chee-kin.lim@dxc.com>
2020-01-03 11:31:30 +0800

    added importer properties and updated data sources configuration

--- src/main/java/teiid/spring/boot/mongodb/odata/DataSources.java
+++ src/main/java/teiid/spring/boot/mongodb/odata/DataSources.java
@@ -1,15 +1,24 @@
 package teiid.spring.boot.mongodb.odata;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.teiid.spring.data.mongodb.MongoDBConfiguration;
 import org.teiid.spring.data.mongodb.MongoDBConnectionFactory;
+import org.teiid.spring.data.mongodb.MongoDBTemplate;
 
 @Configuration
 public class DataSources {
     @Bean
-    public MongoDBConnectionFactory raasagent(@Autowired MongoTemplate template) {
-        return new MongoDBConnectionFactory(template);
+    public MongoDBConnectionFactory raasagent(@Qualifier("config") @Autowired MongoDBConfiguration config) {
+        return new MongoDBConnectionFactory(new MongoDBTemplate(config));
+    }
+
+    @ConfigurationProperties("spring.teiid.data.mongodb.raasagent")
+    @Bean("config")
+    public MongoDBConfiguration mongoConfig() {
+        return new MongoDBConfiguration();
     }
 }
